49dfa1ede5b865081f9ca1cec3bde2253e7eb6a0
divyag <divyag@10.11.2.149>
2018-05-29 14:38:41 +0530

    AddDouble. All commit.

---
+++ AddDouble/src/AddDouble.java
@@ -0,0 +1,14 @@
+
+public class AddDouble {
+
+	public static void main(String[] c) {
+		// TODO Auto-generated method stub
+double num1=4.5;
+double num2=6.3;
+
+System.out.println(num1+num2);
+
+
+	}
+
+}
